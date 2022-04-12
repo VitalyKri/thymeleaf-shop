77e319332b46fdf95afbf7660e9c2d5f013e563b
Vitaly <woodanon123@gmail.com>
2022-04-12 11:01:37 +0500

    lesson 5: add submodule gb-api

---
+++ src/main/java/ru/gb/thymeleafshop/ThymeleafShopApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.thymeleafshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/thymeleafshop/ThymeleafShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.thymeleafshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
