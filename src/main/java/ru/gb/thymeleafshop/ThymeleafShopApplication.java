39436b7bfc128ce553bf57082b46794beb6780ac
Vitaly <woodanon123@gmail.com>
2022-05-14 12:25:45 +0500

    lesson 8

--- src/main/java/ru/gb/thymeleafshop/ThymeleafShopApplication.java
+++ src/main/java/ru/gb/thymeleafshop/ThymeleafShopApplication.java
@@ -2,12 +2,16 @@ package ru.gb.thymeleafshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ru.gb")
 public class ThymeleafShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafShopApplication.class, args);
     }
+
 
 }
